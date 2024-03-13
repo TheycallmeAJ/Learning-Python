6505315b0d1b6b1129430f86ce14f493cc2500c3
TheycallmeAJ <anijas2002@gmail.com>
2024-03-13 18:15:39 +0530

    Lecture6

---
+++ Lecture6.py
@@ -0,0 +1,11 @@
+#function
+#average of 3 nums
+"""def avg(x, y ,z):
+    return (x + y + z)/3
+
+x = float(input('Enter first num: '))
+y = float(input('Enter second num: ')) 
+z = float(input('Enter third num: '))
+print ('Average is ',avg(x, y, z))"""
+
+#
