b373db08d654e73e1ba7c74268318d1991427937
TheycallmeAJ <anijas2002@gmail.com>
2024-03-13 18:11:16 +0530

    Modified

--- Lecture4.py
+++ Lecture4.py
@@ -1,2 +1,24 @@
 #Dictionary and Sets
 
+
+#wap to count the number of students with "A" grade in the following tuple.
+"""grade = ["C", "D", "A", "A", "B", "A"]
+#print(grade.count("A"))
+#store them in a list &sort them from "A" to "D"
+grade.sort()
+print(grade)"""
+
+
+#store the following word meaning in a python dictonary
+"""dict = {
+    "cat" : "a small animal",
+    "table" : ["a piece  of furniture", "list of facts & figures"]
+}
+print(dict)"""
+
+
+#You are given a list of subjects for students. Assume one classroom is required for 1 subject.How many classrooms are needed by all students. Usin set
+"""subjects = {
+    "python", "java", "c++", "python", "javascript", "java", "python", "java", "c++", "c"
+}
+print(len(subjects))"""
