a96fa1f0d511b4906202debc20d6302d26b4d6cb
TheycallmeAJ <anijas2002@gmail.com>
2024-03-13 18:14:01 +0530

    this is PQ for lecture 5

---
+++ Lecture5.py
@@ -0,0 +1,44 @@
+#loop
+
+
+#Print no. from 1 to 100
+"""i = 1
+while i <=100:
+    print(i)
+    i += 1"""
+
+
+#print no from 100 to 1
+"""i = 100
+while i>= 1:
+    print(i)
+    i -=1"""
+
+
+#Print the multiplication table of a number n
+"""n = int(input("Enter any no.: "))
+i = 1
+while i <= 10:
+    print(n*i)
+    i += 1"""
+
+
+#Print the elements of the following list using a loop [1,4,9,16,25,36,49,64,81,100]
+"""nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+idx = 0
+while idx < len(nums):
+    print (nums[idx])
+    idx += 1"""
+
+
+#Search for a num x in this tuple using loop: [1,4,9,16,25,36,49,64,81,100]
+"""nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+x = int(input('Enter a number: '))
+i = 0
+while i < len(nums):
+    if(nums [i] == x):
+        print("Found at idx", i)
+        break
+    else:
+        print('finding...')
+        i += 1"""
